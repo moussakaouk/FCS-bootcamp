e27c60f5cab2674a6f87c12ecb856f9b3bca948a
Moussa Kaouk <moussakaouk123@gmail.com>
2025-09-22 21:46:39 +0300

    assignment 1

---
+++ .history/FCS bootcamp/assignment 1_20250922214501.py
@@ -0,0 +1,23 @@
+lines = 6
+i = 1
+while (i <= lines):
+    j = 1
+    while j < i:
+        print("*")
+        j += 1
+    print()
+    i += 1  
+
+
+    rows = 5  # Number of rows for the pattern
+
+# Outer loop for rows
+i = 1
+while i <= rows:
+    # Inner loop for printing stars in each row
+    j = 1
+    while j <= i:
+        print("*", end=" ")  # Print a star and a space without a newline
+        j += 1
+    print()  # Move to the next line after printing stars for the current row
+    i += 1
---
+++ .history/FCS bootcamp/assignment 1_20250922214547.py
@@ -0,0 +1,9 @@
+lines = 6
+i = 1
+while (i <= lines):
+    j = 1
+    while j <= i:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1  
