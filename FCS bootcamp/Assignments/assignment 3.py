ad07fd4c77933b5d3aae8786d670905b5cb72f5c
Moussa Kaouk <moussakaouk123@gmail.com>
2025-10-08 14:18:06 +0300

    done assignment 3 without functions

--- FCS bootcamp/Assignments/assignment 3.py
+++ FCS bootcamp/Assignments/assignment 3.py
@@ -10,9 +10,34 @@ for i in range(number):
         if 0 <= grade <= 100:
             break
         else:
-            print("Invalid input")
-                       
+            print("Invalid input")       
     student_info = (student, grade)
     students.append((student_info))
                 
 print("students =", students)
+
+print("All students and scores: ")
+
+for i in students:
+    print(i[0],"-",i[1])
+
+print("Average score: ", end="")
+total = 0
+for name, grade in students:
+    total += grade
+
+average = total / len(students)
+
+print(average)
+
+top_student = max(students, key=lambda s: s[1])
+print("Top student:", top_student)
+
+names = []
+for name, grade in students:
+    if grade < 60:
+        names.append(name)
+
+print("Failed students: ", end="")
+for name in names:
+    print(name, end=", ")
